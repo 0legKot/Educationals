2387dba9ed32cbc78ac33cfaee7af90a88d4b316
0legKot <kronosgod12@gmail.com>
2024-11-18 20:52:46 +0200

    Add project files.

---
+++ Project1/Components/DbPediaClient.cs
@@ -0,0 +1,26 @@
+﻿using VDS.RDF.Query;
+
+namespace Project1.Components {
+    public class DbPediaClient {
+        private static SparqlQueryClient? client;
+
+        private static SparqlQueryClient Client {
+            get {
+                if (client == null) {
+                    var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                    httpClient.DefaultRequestHeaders.Add("Host", "dbpedia.org");
+                    httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+                    client = new SparqlQueryClient(httpClient, new Uri("http://dbpedia.org/sparql"));
+                }
+                return client;
+            }
+        }
+        public static async Task<SparqlResultSet> Get(string query) {
+            
+            SparqlResultSet results = await Client.QueryWithResultSetAsync("select distinct ?Concept where {[] a ?Concept} LIMIT 10");
+            return results;
+        }
+
+    }
+}
